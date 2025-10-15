0486547e87b4f8ad86b7f593c8800cc8a4c42ec6
Olivier De Jonckere <olivier.de-jonckere@lirmm.fr>
2025-10-15 14:35:41 +0200

    Fix example name

--- exercises/0-ion-tvgutil-parsing/0-ion-tvgutil-parsing.rs
+++ exercises/0-ion-tvgutil-parsing/0-ion-tvgutil-parsing.rs
@@ -7,7 +7,8 @@
 fn main() {
     // Exo 1: retrieve and display the contacts from the examples/0-parsing/contact_plan.ion file
 
-    //let cp_ion = "exercises/0-parsing/contact_plan.ion";
+    // Use that
+    // let cp_ion = "exercises/0-parsing/contact_plan.ion";
 
     // Use the "NoManagement" type for the node managers.
     // Use the "EVLManager" for the contacts managers. (EVL as defined in SABR)
@@ -17,10 +18,10 @@ fn main() {
     // Display Nodes and Contacts with the {:?} (standard) or {:#?} (pettry print) formats
     // Example: println!("{:?}", node);
 
-    // cargo run --example 0-parsing  # Expected to fail
-    // cargo run --example 0-parsing --features=debug
-    // cargo run --example 0-parsing --features=debug,contact_work_area
-    // cargo run --example 0-parsing --features=debug,contact_suppression,first_depleted
+    // cargo run --example 0-ion-tvgutil-parsing  # Expected to fail
+    // cargo run --example 0-ion-tvgutil-parsing --features=debug
+    // cargo run --example 0-ion-tvgutil-parsing --features=debug,contact_work_area
+    // cargo run --example 0-ion-tvgutil-parsing --features=debug,contact_suppression,first_depleted
 
     // Those different compilation options should already be reflected in the standard output of the contacts
 }
