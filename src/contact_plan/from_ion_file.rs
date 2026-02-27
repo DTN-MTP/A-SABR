30ba303cfe12a0ff8ee5089411c538e86640ab11
Théo Tchilinguirian <theo.tchlx@proton.me>
2026-02-27 13:20:15 +0100

    refactor: use if-let-chain pattern
    A previous PR (#19) refactors the code with if-let guards.
    
    Since Rust edition 2024, if-let chains are possible.
    This commit updates the code to use this newer pattern for better
    readability.

--- src/contact_plan/from_ion_file.rs
+++ src/contact_plan/from_ion_file.rs
@@ -244,20 +244,18 @@ impl IONContactPlan {
         }
 
         for range in &ranges {
-            let Some(tx_map) = contact_info_map.get_mut(&range.tx_node) else {
-                continue;
-            };
-            let Some(contact_vec) = tx_map.get_mut(&range.rx_node) else {
-                continue;
-            };
-            for contact in contact_vec.iter_mut() {
-                if range.tx_start <= contact.tx_start && contact.tx_end <= range.tx_end {
-                    contact.delay = range.delay;
-                    contacts.push(CM::ion_convert(contact).unwrap());
-                } else {
-                    return Err(ASABRError::ContactPlanError(
-                        "This parser only supports one range per contact",
-                    ))?;
+            if let Some(tx_map) = contact_info_map.get_mut(&range.tx_node)
+                && let Some(contact_vec) = tx_map.get_mut(&range.rx_node)
+            {
+                for contact in contact_vec.iter_mut() {
+                    if range.tx_start <= contact.tx_start && contact.tx_end <= range.tx_end {
+                        contact.delay = range.delay;
+                        contacts.push(CM::ion_convert(contact).unwrap());
+                    } else {
+                        return Err(ASABRError::ContactPlanError(
+                            "This parser only supports one range per contact",
+                        ))?;
+                    }
                 }
             }
         }
