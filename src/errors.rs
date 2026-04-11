1707880888b2ae172c584c4ee6f63b8d8dd1545f
Théo Tchilinguirian <theo.tchlx@proton.me>
2026-04-11 20:54:00 +0200

    feat(errors.rs): introduce CowError
    This commit introduces `CowError(Cow<'static, str>)`, a new error type
    that's optimized for cases where the error is a message, that may be a
    `&'static str` or a `String`.
    
    If it's a String, such as one generated by `format!()`, it will be
    cloned and owned. Otherwise it will be borrowed.

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+use std::borrow::Cow;
 use std::cell::BorrowMutError;
 use std::error::Error;
 use std::fmt;
@@ -30,3 +31,29 @@ impl From<ASABRError> for std::io::Error {
         std::io::Error::other(err)
     }
 }
+
+#[derive(Debug)]
+pub struct CowError(Cow<'static, str>);
+
+impl CowError {
+    /// Borrows `'a str`s, clones and owns `String`s as needed.
+    ///
+    /// Usage:
+    ///
+    /// * No allocation for static messages:
+    ///   * `return Err(ASABRError::ParsingError(CowError::new("A static error message")));`
+    ///
+    /// * Allocates and owns a dynamically determined message:
+    ///   * `return Err(ASABRError::ParsingError(CowError::new(format!("A dynamic error: {context}"))));`
+    pub fn new(msg: impl Into<Cow<'static, str>>) -> Self {
+        Self(msg.into())
+    }
+}
+
+impl fmt::Display for CowError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for CowError {}
