894c8120f417116b636480ba430fcac723870697
Mustafa Ben Mansour <mustafa.ben-mansour@etu.umontpellier.fr>
2026-07-09 17:05:31 +0200

    impl node_manager for storagenodemanager

--- asabr/src/node_manager/node_manager.rs
+++ asabr/src/node_manager/node_manager.rs
@@ -20,131 +20,131 @@ impl StorageNodeManager {
         }
     }
         fn can_reserve(
-        &self,
-        interval: TimeInterval,
-        volume: Volume,
-    ) -> bool {
+            &self,
+            interval: TimeInterval,
+            volume: Volume,
+            ) -> bool {
 
-        let mut events: Vec<(Date, Volume)> = Vec::new();
+            let mut events: Vec<(Date, Volume)> = Vec::new();
 
-        for (reserved, used) in &self.reservations {
+            for (reserved, used) in &self.reservations {
 
-            if reserved.end <= interval.start || reserved.start >= interval.end {
-                continue;
+                if reserved.end <= interval.start || reserved.start >= interval.end {
+                    continue;
+                }
+
+                let start = reserved.start.max(interval.start);
+                let end = reserved.end.min(interval.end);
+
+                events.push((start, *used));
+                events.push((end, -*used));
             }
 
-            let start = reserved.start.max(interval.start);
-            let end = reserved.end.min(interval.end);
+            events.push((interval.start, volume));
+            events.push((interval.end, -volume));
 
-            events.push((start, *used));
-            events.push((end, -*used));
-        }
+            events.sort_by_key(|e| e.0);
 
-        events.push((interval.start, volume));
-        events.push((interval.end, -volume));
+            let mut current = 0;
 
-        events.sort_by_key(|e| e.0);
+            for (_, delta) in events {
+                current += delta;
 
-        let mut current = 0;
-
-        for (_, delta) in events {
-            current += delta;
-
-            if current > self.capacity {
-                return false;
+                if current > self.capacity {
+                    return false;
+                }
             }
-        }
 
-        true
-    }
+            true
+        }
 }
 impl NodeManager for StorageNodeManager {
 
     fn accept(
-    &self,
-    bundle: &Bundle,
-    _time: TimeInterval,
-    _sender: NodeID,
-) -> bool {
-    bundle.size <= self.capacity
-}
+        &self,
+        bundle: &Bundle,
+        _time: TimeInterval,
+        _sender: NodeID,
+    ) -> bool {
+        bundle.size <= self.capacity
+    }
 
     fn delay(
-    &self,
-    _bundle: &Bundle,
-    reception: TimeInterval,
-    _sender: NodeID,
-    _next: NodeID,
-) -> Date {
-    reception.end
-}
+        &self,
+        _bundle: &Bundle,
+        reception: TimeInterval,
+        _sender: NodeID,
+        _next: NodeID,
+    ) -> Date {
+        reception.end
+    }
 
     fn dry_run_retention(
-    &self,
-    bundle: &Bundle,
-    reception: TimeInterval,
-    _sender: NodeID,
-    transmission: TimeInterval,
-    _next: NodeID,
-) -> bool {
+        &self,
+        bundle: &Bundle,
+        reception: TimeInterval,
+        _sender: NodeID,
+        transmission: TimeInterval,
+        _next: NodeID,
+    ) -> bool {
 
-    let retention = TimeInterval {
-        start: reception.start,
-        end: transmission.start,
-    };
+        let retention = TimeInterval {
+            start: reception.start,
+            end: transmission.start,
+        };
 
-    self.can_reserve(retention, bundle.size)
-}
+        self.can_reserve(retention, bundle.size)
+    }
 
     fn dry_run_multi(
-    &self,
-    bundle: &Bundle,
-    reception: TimeInterval,
-    _sender: NodeID,
-    transmissions: &[(TimeInterval, NodeID)],
-) -> Option<usize> {
+        &self,
+        bundle: &Bundle,
+        reception: TimeInterval,
+        _sender: NodeID,
+        transmissions: &[(TimeInterval, NodeID)],
+    ) -> Option<usize> {
 
-    let end = transmissions
-        .iter()
-        .map(|(t, _)| t.start)
-        .max()
-        .unwrap_or(reception.end);
+        let end = transmissions
+            .iter()
+            .map(|(t, _)| t.start)
+            .max()
+            .unwrap_or(reception.end);
 
-    let retention = TimeInterval {
-        start: reception.start,
-        end,
-    };
-
-    if self.can_reserve(retention, bundle.size) {
-        Some(transmissions.len())
-    } else {
-        None
-    }
-}
-
-    fn commit(
-    &mut self,
-    bundle: &Bundle,
-    reception: TimeInterval,
-    _sender: NodeID,
-    transmissions: &[(TimeInterval, NodeID)],
-) -> Result<(), ASABRError> {
-
-    let end = transmissions
-        .iter()
-        .map(|(t, _)| t.start)
-        .max()
-        .unwrap_or(reception.end);
-
-    self.reservations.push((
-        TimeInterval {
+        let retention = TimeInterval {
             start: reception.start,
             end,
-        },
-        bundle.size,
-    ));
+        };
 
-    Ok(())
-}
+        if self.can_reserve(retention, bundle.size) {
+            Some(transmissions.len())
+        } else {
+            None
+        }
+    }
+
+    fn commit(
+        &mut self,
+        bundle: &Bundle,
+        reception: TimeInterval,
+        _sender: NodeID,
+        transmissions: &[(TimeInterval, NodeID)],
+    ) -> Result<(), ASABRError> {
+
+        let end = transmissions
+            .iter()
+            .map(|(t, _)| t.start)
+            .max()
+            .unwrap_or(reception.end);
+
+        self.reservations.push((
+            TimeInterval {
+                start: reception.start,
+                end,
+            },
+            bundle.size,
+        ));
+
+        Ok(())
+    }
 
 }
