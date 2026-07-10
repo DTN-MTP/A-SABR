e2b630cf374e9d0debb91ad7e042f442e671c5c7
Mustafa Ben Mansour <mustafa.ben-mansour@etu.umontpellier.fr>
2026-07-10 18:03:24 +0200

    correction implementation

--- asabr/src/node_manager/node_manager.rs
+++ asabr/src/node_manager/node_manager.rs
@@ -9,65 +9,46 @@ use crate::{
 
 #[derive(Debug, Clone)]
 pub struct StorageNodeManager {
-    reservations: Vec<(TimeInterval, Volume)>,
+    /// Memory occupation:
+    /// (date, volume occupied from this date until the next change)
+    memory: Vec<(Date, Volume)>,
     capacity: Volume,
 }
 impl StorageNodeManager {
     pub fn new(capacity: Volume) -> Self {
         Self {
-            reservations: Vec::new(),
+            memory: Vec::new(),
             capacity,
         }
     }
-        fn can_reserve(
-            &self,
-            interval: TimeInterval,
-            volume: Volume,
-            ) -> bool {
 
-            let mut events: Vec<(Date, Volume)> = Vec::new();
 
-            for (reserved, used) in &self.reservations {
-
-                if reserved.end <= interval.start || reserved.start >= interval.end {
-                    continue;
-                }
-
-                let start = reserved.start.max(interval.start);
-                let end = reserved.end.min(interval.end);
-
-                events.push((start, *used));
-                events.push((end, -*used));
-            }
-
-            events.push((interval.start, volume));
-            events.push((interval.end, -volume));
-
-            events.sort_by_key(|e| e.0);
-
-            let mut current = 0;
-
-            for (_, delta) in events {
-                current += delta;
-
-                if current > self.capacity {
-                    return false;
-                }
-            }
-
-            true
-        }
-}
 impl NodeManager for StorageNodeManager {
 
     fn accept(
         &self,
         bundle: &Bundle,
-        _time: TimeInterval,
+        reception: TimeInterval,
         _sender: NodeID,
     ) -> bool {
-        bundle.size <= self.capacity
-    }
+
+        let mut result = self.memory[self.memory.len() - 1].1;
+
+        let mut idx = 0;
+        while idx < self.memory.len() && self.memory[idx].0 < reception.start {
+            idx+=1;
+        }
+
+        while idx < self.memory.len() && self.memory[idx].0 <= reception.end {
+            
+            if self.memory[idx].1 + bundle.size > self.capacity {
+                return false;
+            }
+            idx+=1;
+        }
+
+        return true
+     }
 
     fn delay(
         &self,
@@ -79,7 +60,7 @@ impl NodeManager for StorageNodeManager {
         reception.end
     }
 
-    fn dry_run_retention(
+    fn dry_run_retention( //on vérifie pour toutes les intervalle de temps avant le release si la taille du bundle ajoutée à la somme des réservations existantes dépasse la capacité du nœud. Si c'est le cas, on retourne false, sinon true.
         &self,
         bundle: &Bundle,
         reception: TimeInterval,
@@ -88,12 +69,22 @@ impl NodeManager for StorageNodeManager {
         _next: NodeID,
     ) -> bool {
 
-        let retention = TimeInterval {
-            start: reception.start,
-            end: transmission.start,
-        };
+        let mut result = self.memory[self.memory.len() - 1].1;
 
-        self.can_reserve(retention, bundle.size)
+        let mut idx = 0;
+        while idx < self.memory.len() && self.memory[idx].0 < reception.start {
+            idx+=1;
+        }
+
+        while idx < self.memory.len() && self.memory[idx].0 <= transmission.end {
+            
+            if self.memory[idx].1 + bundle.size > self.capacity {
+                return false;
+            }
+            idx+=1;
+        }
+
+        return true
     }
 
     fn dry_run_multi(
@@ -104,20 +95,27 @@ impl NodeManager for StorageNodeManager {
         transmissions: &[(TimeInterval, NodeID)],
     ) -> Option<usize> {
 
-        let end = transmissions
+        let release = transmissions
             .iter()
-            .map(|(t, _)| t.start)
+            .map(|(interval, _)| interval.start)
             .max()
             .unwrap_or(reception.end);
 
-        let retention = TimeInterval {
-            start: reception.start,
-            end,
-        };
+        let retention = (
+            reception.start,
+            release,
+    );
 
-        if self.can_reserve(retention, bundle.size) {
+        if self.simulate_retention(
+            retention.start,
+            release,
+            bundle.size,
+        ) {
+
             Some(transmissions.len())
+
         } else {
+
             None
         }
     }
@@ -130,21 +128,28 @@ impl NodeManager for StorageNodeManager {
         transmissions: &[(TimeInterval, NodeID)],
     ) -> Result<(), ASABRError> {
 
-        let end = transmissions
-            .iter()
-            .map(|(t, _)| t.start)
-            .max()
-            .unwrap_or(reception.end);
 
-        self.reservations.push((
-            TimeInterval {
-                start: reception.start,
-                end,
-            },
-            bundle.size,
-        ));
+        let mut result = self.memory[self.memory.len() - 1].1;
 
-        Ok(())
+        let mut idx = 0;
+        while idx < self.memory.len() && self.memory[idx].0 < reception.start {
+            idx+=1;
+        }
+        
+        if reception.start < self.memory[idx].0 {
+            self.memory.insert(idx, (reception.start, self.memory[idx - 1].1));
+        }
+
+        while idx < self.memory.len() && transmission.end > self.memory[idx].0 {
+                self.memory[idx].1 += bundle.size;
+                idx+= 1;
+        }
+
+        if transmission.end < self.memory[idx].0 {
+            self.memory.insert(idx, (transmission.end, self.memory[idx - 1].1 - bundle.size));
+        }
+
     }
-
+    
+}
 }
